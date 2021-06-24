3c26aea1dd423cffa93400d247adc95e1e274e5b
spatel051 <77207491+spatel051@users.noreply.github.com>
2021-06-24 13:52:49 -0500

    Update Atm.py

--- Atm.py
+++ Atm.py
@@ -1,5 +1,4 @@
 class Atm(object):
-
     def __init__(self, card_num, pin_num):
         self.card_num = card_num
         self.pin_num = pin_num
@@ -7,8 +6,23 @@ class Atm(object):
     def cash_with_drawl(self, amount):
         print("withdrawing...")
     
-    def balance_enquiry(self):
+    def check_balance(self):
         print("Please wait while we get your balance")
-    
-    def deposit(self, amount):
-        print("Depositing")
+
+def main():
+    card_number = input("Insert your card number:- ")
+    pin_number = input("Enter your pin number:- ")
+    new_user = Atm(card_num, pin_num)
+
+    print("Choose your activity ")
+    print("1.Balance Enquriy   2.Withdrawl")
+    activity = int(input("enter activity number :- "))
+
+    if (activity == 1):
+        new_user.check_balance()
+    elif (activity == 2):
+        amount = int(input("Enter the amount:- "))
+        new_user.withdrawl(amount)
+    else:
+        print("Enter a valid number")
+main()
